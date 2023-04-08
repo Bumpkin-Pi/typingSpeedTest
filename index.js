d2cfdcc2e6023ef0a728e145c3d4829f52533fe1
BumpkinPi <h.person.notrobot@gmail.com>
2023-04-08 18:12:18 +0100

    Rebroke it

---
+++ index.js
@@ -0,0 +1,74 @@
+
+class Text{
+    text = "Hello there i would like to give you a chance to give me an opertunity to give me a fish";
+    text_buffer = this.text;
+    current_pos = 0;
+    current_word = 0;
+    user_text = ""
+    user_word = ""
+
+    take_char(user_text){
+        let buffer = "";
+        let buffer_array = this.text.split(" ").slice(this.current_word, this.text.split(" ").length-1)
+        user_text.trim();
+
+
+        let input_array = user_text.split(" ");
+
+        this.current_word = (input_array.length)-1;
+        console.log(this.current_word)
+
+
+        this.text_buffer = ""
+        for (let i in buffer_array){
+            this.text_buffer += buffer_array[i]+" ";
+            console.log(buffer_array[i])
+
+        }
+
+
+        let user_words = user_text.split(" ");
+        if (user_words[user_words.length-1] == ""){
+            user_words.pop();
+        }
+        for (let word in user_words){
+            console.log(user_words[word])
+
+        }
+
+
+
+
+
+
+
+        console.log(user_text)
+
+        let correctchar = this.text_buffer[0];
+
+        // this.text_buffer = this.text_buffer.replace(this.text_buffer[0], "");
+        // this.text_buffer[0] = this.text_buffer[0].replace(" ", "_")
+        document.getElementById("text_area").innerText = this.text_buffer//.replace(" ", "_")
+
+
+
+
+    }
+
+}
+let text = new Text();
+document.getElementById("text_area").innerText = text.text_buffer
+
+
+
+function onInput(event){
+    let current_user_text = document.getElementById("input_area").value;
+    user_word = current_user_text.split(" ")[current_user_text.split(" ").length - 1]
+    console.log(user_word)
+    text.take_char(current_user_text)
+
+
+}
+
+
+
