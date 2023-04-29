51a7106707f21111a6054fef9e8287776af898e5
holly <h.person.notrobot@gmail.com>
2023-04-29 12:53:57 +0100

    Added WPM counter

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
-const timer_max = 10;
-
+play_sound = true
 
 class Text{
-    text = "Hello there i would like to give you a chance to give me an opertunity to give me a fish".toLowerCase();
+    text = "Hello there i would like to give you a chance to give me an opertunity to give me a fish. I would also like to tell you how greatfull i am that you could join us today, it honors me greatly to have such guests, truly very fun frankly. I cant say how mildly happy I am, really, its very exiting. But that winded wounded, not even dead, tonight in jungle land".toLowerCase();
     text_buffer = this.text;
     current_pos = 0;
     current_word = 0;
@@ -45,14 +43,19 @@ class Text{
         // this.text_buffer = this.text_buffer.replace(this.text_buffer[0], "");
         // this.text_buffer[0] = this.text_buffer[0].replace(" ", "_")
         document.getElementById("text_area").innerText = this.text_buffer//.replace(" ", "_")
+
     }
 
 }
+const timer_max = 60;
 let text = new Text();
 document.getElementById("text_area").innerText = text.text_buffer
 let countdown = new Date().getTime()+60;
 
 let playing = false;
+
+
+
 
 function onInput(event){
     if (!playing && time !== 0){
@@ -60,6 +63,9 @@ function onInput(event){
         playing = true;
     }
     if (time !== 0){
+        if (play_sound){
+
+        }
         let current_user_text = document.getElementById("input_area").value;
         user_word = current_user_text.split(" ")[current_user_text.split(" ").length - 1]
         text.take_char(current_user_text)
@@ -78,24 +84,14 @@ let x = setInterval(function() {
         }
     }
     document.getElementById("time").innerText = "Time: "+time+"s"
+    calc_wpm()
 }, 1000);
 
 
-// function set_accuracy(){
-//     let input_array = document.getElementById("input_area").value.split(" ");
-//
-//     document.getElementById("input_area").value;
-//     user_word = current_user_text.split(" ")[current_user_text.split(" ").length - 1]
-//
-//
-//     this.current_word = (input_array.length)-1;
-//     let buffer_array = this.text.split(" ").slice(this.current_word)
-//     buffer_array[0] = buffer_array[0].replace(input_array[input_array.length-1], "")
-//
-//
-//
-//
-// }
-
+function calc_wpm(){
+    document.getElementById("wpm").innerText = "WMP: "+(
+        Math.floor((text.current_word/(timer_max-time))*60)
+    )
+}
 
 
